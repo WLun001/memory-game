79405676f0b57a927065bcd9d8a4f3ddac08703b
Wei Lun <weilun_95@hotmail.com>
2018-06-27 23:59:06 +0800

    first commit

---
+++ MemoryGame/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MemoryGame
+//
+//  Created by Wei Lun on 27/06/2018.
+//  Copyright © 2018 Wei Lun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var wordLabel1: UILabel!
+    @IBOutlet weak var wordLabel2: UILabel!
+    @IBOutlet weak var wordLabel3: UILabel!
+    @IBOutlet weak var wordLabel4: UILabel!
+    @IBOutlet weak var wordLabel5: UILabel!
+    @IBOutlet weak var wordLabel6: UILabel!
+    @IBOutlet weak var wordLabel7: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MemoryGame/Words.swift
@@ -0,0 +1,53 @@
+//
+//  Words.swift
+//  MemoryGame
+//
+//  Created by Wei Lun on 27/06/2018.
+//  Copyright © 2018 Wei Lun. All rights reserved.
+//
+
+import Foundation
+
+class Words {
+    var words = [String]()
+    
+    init() {
+        words = [
+            "awkwardish",
+            "carbonado",
+            "chamaephyte",
+            "chroococcaceous",
+            "coracoscapular",
+            "counterborder",
+            "dialogize",
+            "diluvia",
+            "economise",
+            "gnast",
+            "graceful",
+            "histiocytic",
+            "jaguarundi",
+            "kalymmaukion",
+            "keramic",
+            "kinkily",
+            "laicized",
+            "misthrows",
+            "mosasauroid",
+            "niggling",
+            "outthrown",
+            "overcreed",
+            "panneuritic",
+            "pinweed",
+            "pleuropulmonary",
+            "recharging",
+            "redfinch",
+            "scotswoman",
+            "smithery",
+            "soarings",
+            "sternopericardial",
+            "stickful",
+            "tiatinagua",
+            "unjuicy",
+            "veratrias"
+        ]
+    }
+}
