1dd48de2b85a13bd8dd5947e9f09fb2e170bc7a6
Wei Lun <weilun_95@hotmail.com>
2018-06-28 00:43:43 +0800

    added word label

--- MemoryGame/ViewController.swift
+++ MemoryGame/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labels: [UILabel]!
     @IBOutlet weak var wordLabel1: UILabel!
     @IBOutlet weak var wordLabel2: UILabel!
     @IBOutlet weak var wordLabel3: UILabel!
@@ -17,14 +18,34 @@ class ViewController: UIViewController {
     @IBOutlet weak var wordLabel5: UILabel!
     @IBOutlet weak var wordLabel6: UILabel!
     @IBOutlet weak var wordLabel7: UILabel!
+    
+    private let words = Words().words
+    private var counter = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        labels.append(wordLabel1)
+        labels.append(wordLabel2)
+        labels.append(wordLabel3)
+        labels.append(wordLabel4)
+        labels.append(wordLabel5)
+        labels.append(wordLabel6)
+        labels.append(wordLabel7)
+        
+        initLabel()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func initLabel() {
+        for label in labels {
+            label.text = words[counter]
+            counter += 1
+        }
     }
 
 
